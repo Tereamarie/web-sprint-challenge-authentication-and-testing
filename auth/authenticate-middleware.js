1673862540cc93dd473edb7b23fed44ba7fd6cc2
marie-hotchkiss <mtknowlton67@gmail.com>
2020-08-16 19:17:28 -0700

    changed api/ created users-model

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,25 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets.js');
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+
+  const token= req.headers.authorization;
+  
+  if (token) {
+    jwt.verify(token, secrets.jwtSecret, (error, decodedToken) => {
+      if (error) {
+        res.status(401).json({message: "whoops, sorry friend"});
+      } else {
+        req.decodedJwt = decodedToken;
+        console.log(req.decodedJwt);
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
+  
 };
